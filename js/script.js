240ca48bdcecbeb3ce4ec4d48a3b8dd204ce4cdf
Maciej Łata <maciejcezarylata@gmail.com>
2021-05-19 23:50:19 +0200

    working under problematics

--- js/script.js
+++ js/script.js
@@ -3,46 +3,41 @@ function welcome() {
 }
 welcome();
 
-let sectionAbstract__hidden = document.querySelector(".sectionAbstract__hidden");
-let sectionAbstract__dots = document.querySelector(".sectionAbstract__dots");
-let sectionAbstract__moreButton = document.querySelector(".sectionAbstract__moreButton");
-let sectionAbstract__readMore = document.querySelector(".sectionAbstract__readMore");
 
 
-let sectionProblematics__hidden = document.querySelector(".sectionProblematics__hidden");
-let sectionProblematics__dots = document.querySelector(".sectionProblematics__dots");
-let sectionProblematics__moreButton = document.querySelector(".sectionProblematics__moreButton");
-let sectionProblematics__readMore = document.querySelector(".sectionProblematics__readMore");
+const sectionForm__hidden = document.querySelector(".sectionForm__hidden");
+const sectionForm__dots = document.querySelector(".sectionForm__dots");
+const sectionForm__moreButton = document.querySelector(".sectionForm__moreButton");
+const sectionForm__readMore = document.querySelector(".sectionForm__readMore");
 
 
-let sectionForm__hidden = document.querySelector(".sectionForm__hidden");
-let sectionForm__dots = document.querySelector(".sectionForm__dots");
-let sectionForm__moreButton = document.querySelector(".sectionForm__moreButton");
-let sectionForm__readMore = document.querySelector(".sectionForm__readMore");
-
-
-let sectionIdea__hidden = document.querySelector(".sectionIdea__hidden");
-let sectionIdea__dots = document.querySelector(".sectionIdea__dots");
-let sectionIdea__moreButton = document.querySelector(".sectionIdea__moreButton");
-let sectionIdea__readMore = document.querySelector(".sectionIdea__readMore");
+const sectionIdea__hidden = document.querySelector(".sectionIdea__hidden");
+const sectionIdea__dots = document.querySelector(".sectionIdea__dots");
+const sectionIdea__moreButton = document.querySelector(".sectionIdea__moreButton");
+const sectionIdea__readMore = document.querySelector(".sectionIdea__readMore");
 
 
 
-let sectionInspirations__photo2 = document.querySelector(".sectionInspirations__photo2");
-let sectionInspirations__photo3 = document.querySelector(".sectionInspirations__photo3");
-let sectionInspirations__photo4 = document.querySelector(".sectionInspirations__photo4");
-let sectionInspiration__hidden = document.querySelector(".sectionInspiration__hidden");
-let sectionInspirations__showButton2 = document.querySelector(".sectionInspirations__showButton2");
-let sectionInspirations__showButton3 = document.querySelector(".sectionInspirations__showButton3");
-let sectionInspirations__showButton4 = document.querySelector(".sectionInspirations__showButton4");
+const sectionInspirations__showButton1 = document.querySelector(".sectionInspirations__showButton1");
+const sectionInspirations__showButton2 = document.querySelector(".sectionInspirations__showButton2");
+const sectionInspirations__showButton3 = document.querySelector(".sectionInspirations__showButton3");
+const sectionInspirations__showButton4 = document.querySelector(".sectionInspirations__showButton4");
+const sectionAbstract__moreButton = document.querySelector(".sectionAbstract__moreButton");
+const sectionProblematics__moreButton = document.querySelector(".sectionProblematics__moreButton");
 
 
-let footer__showAuthor = document.querySelector(".footer__showAuthor");
+
+
+
+
+
+
+const footer__showAuthor = document.querySelector(".footer__showAuthor");
 
 
 const visibilityInspirationOne = () => {
-    let sectionInspirations__photo1 = document.querySelector(".sectionInspirations__photo1");
-    let sectionInspirations__showButton1 = document.querySelector(".sectionInspirations__showButton1");
+    const sectionInspirations__photo1 = document.querySelector(".sectionInspirations__photo1");
+    const sectionInspirations__showButton1 = document.querySelector(".sectionInspirations__showButton1");
 
     sectionInspirations__photo1.classList.toggle("sectionInspirations__active");
     if (sectionInspirations__showButton1.innerText == "Pokaż") {
@@ -52,54 +47,89 @@ const visibilityInspirationOne = () => {
     }
 };
 
-sectionInspirations__showButton1.addEventListener("click",visibilityInspirationOne);
 
-sectionInspirations__showButton2.addEventListener("click", () => {
+const visibilityInspirationTwo = () => {
+    const sectionInspirations__photo2 = document.querySelector(".sectionInspirations__photo2");
+    const sectionInspirations__showButton2 = document.querySelector(".sectionInspirations__showButton2");
+
     sectionInspirations__photo2.classList.toggle("sectionInspirations__active");
     if (sectionInspirations__showButton2.innerText == "Pokaż") {
         sectionInspirations__showButton2.innerText = "Ukryj";
     } else {
         sectionInspirations__showButton2.innerText = "Pokaż";
     }
-});
+};
 
-sectionInspirations__showButton3.addEventListener("click", () => {
+
+const visibilityInspirationThree = () => {
+    const sectionInspirations__photo3 = document.querySelector(".sectionInspirations__photo3");
+    const sectionInspirations__showButton3 = document.querySelector(".sectionInspirations__showButton3");
+
     sectionInspirations__photo3.classList.toggle("sectionInspirations__active");
     if (sectionInspirations__showButton3.innerText == "Pokaż") {
         sectionInspirations__showButton3.innerText = "Ukryj";
     } else {
         sectionInspirations__showButton3.innerText = "Pokaż";
     }
-});
+};
 
-sectionInspirations__showButton4.addEventListener("click", () => {
+
+const visibilityInspirationFour = () => {
+    const sectionInspirations__photo4 = document.querySelector(".sectionInspirations__photo4");
+    const sectionInspirations__showButton4 = document.querySelector(".sectionInspirations__showButton4");
+
     sectionInspirations__photo4.classList.toggle("sectionInspirations__active");
     if (sectionInspirations__showButton4.innerText == "Pokaż") {
         sectionInspirations__showButton4.innerText = "Ukryj";
     } else {
         sectionInspirations__showButton4.innerText = "Pokaż";
     }
-});
+};
 
 
+const rollAbstract = () => {
+    const sectionAbstract__hidden = document.querySelector(".sectionAbstract__hidden");
+    const sectionAbstract__dots = document.querySelector(".sectionAbstract__dots");
+    const sectionAbstract__moreButton = document.querySelector(".sectionAbstract__moreButton");
 
-
-
-
-sectionAbstract__moreButton.addEventListener("click", () => {
-    sectionAbstract__hidden.classList.toggle("sectionAbstract__onText");
-    sectionAbstract__dots.classList.toggle("sectionAbstract__hidden");
+    sectionAbstract__dots.classList.toggle("sectionAbstract__hidden") &&
+        sectionAbstract__hidden.classList.toggle("sectionAbstract__onText");
 
     sectionAbstract__moreButton.innerText = sectionAbstract__dots.classList.contains("sectionAbstract__hidden") ? "Zwiń" : "Czytaj wiecej";
-});
+};
 
 
-sectionProblematics__moreButton.addEventListener("click", () => {
+const rollProblematics = () => {
+    const sectionProblematics__hidden = document.querySelector(".sectionProblematics__hidden");
+    const sectionProblematics__dots = document.querySelector(".sectionProblematics__dots");
+    const sectionProblematics__moreButton = document.querySelector(".sectionProblematics__moreButton");
+
     sectionProblematics__hidden.classList.toggle("sectionProblematics__onText");
     sectionProblematics__dots.classList.toggle("sectionProblematics__hidden");
 
     sectionProblematics__moreButton.innerText = sectionProblematics__dots.classList.contains("sectionProblematics__hidden") ? "Zwiń" : "Czytaj wiecej";
-});
+};
+
+
+const init = () => {
+
+
+    sectionInspirations__showButton1.addEventListener("click", visibilityInspirationOne);
+    sectionInspirations__showButton2.addEventListener("click", visibilityInspirationTwo);
+    sectionInspirations__showButton3.addEventListener("click", visibilityInspirationThree);
+    sectionInspirations__showButton4.addEventListener("click", visibilityInspirationFour);
+    sectionAbstract__moreButton.addEventListener("click", rollAbstract);
+    sectionProblematics__moreButton.addEventListener("click", rollProblematics);
+}
+init();
+
+
+
+
+
+
+
+
 
 
 sectionForm__moreButton.addEventListener("click", () => {
